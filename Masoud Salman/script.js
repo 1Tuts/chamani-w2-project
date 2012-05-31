07c8493314f0f2556ba74e664c00c3d903cf2559
Masoud Salman <goldgoldy276@gmail.com>
2012-05-31 20:48:59 +0430

    add jq to web page :D !

--- Masoud Salman/script.js
+++ Masoud Salman/script.js
@@ -1,4 +1,4 @@
-window.onload = function (){
+/*window.onload = function (){
 	var bck = document.getElementsByTagName("button").item(0);
 	var nxt = document.getElementsByTagName("button").item(1);
 	var scrol = document.getElementsByClassName("scroll").item(0);
@@ -9,3 +9,27 @@ window.onload = function (){
 		scrol.style.left+=-1900+"px";
 	}	
 }
+*/
+$(function(){
+
+$("div.back button").click(function()
+{
+	$(".scroll").animate({
+		'left':'0'		
+		},700,function(){
+			$(".scroll .rgt,.veiw").animate({
+			'height':'1280px'
+			},2000);
+	});
+});
+$("div.next button").click(function(){
+	$(".scroll").animate({
+		'left':'-1900px'		
+		},700,function(){
+			$(".scroll .lft,.veiw").animate({
+			'height':'1280px'
+			},2000);
+	});
+});
+
+});
