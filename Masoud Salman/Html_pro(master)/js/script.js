1716038e4deb320c9282e2e82acb6f6ddd53b2b9
Masoud Salman <goldgoldy276@gmail.com>
2012-06-22 18:25:06 +0430

    change for better than previous code !

--- Masoud Salman/Html_pro(master)/js/script.js
+++ Masoud Salman/Html_pro(master)/js/script.js
@@ -1,12 +1,11 @@
 $(function(){
 /* /////////////////////////////////// Start Top jq /////////////////////////////////// */
-	var top = $('.top');
 	var button = $('.circle-three .button div');
+	var top    = $('.top');
 
 	top.mouseover(function(){
-		$(this).stop().animate({'top':'-290px'},500,'easeOutBounce')
-		button.css({'display':'block'});
-		button.stop().animate({'height':'92px'},400,'easeInOutCirc');
+		$(this).stop().animate({'top':'-290px'},500,'easeOutBounce');
+		 button.stop().animate({'height':'92px','display':'block'},400,'easeInOutCirc');
 		$('.text2').stop().animate({'opacity': '0'},300,null,function(){
 			$('.text2').css({'display':'none'});
 		});
@@ -14,15 +13,12 @@ $(function(){
 	});
 	top.mouseout(function(){
 		$(this).stop().animate({'top':'-440px'},800,'easeInOutElastic');
-		button.stop().animate({'height':'0'},300,'easeInOutCirc',function(){
-			button.css({'display':'none'});
-			$('.text2').css({'display':'block'});
-			$('.text2').stop().animate({'opacity': '1'},300);
+		button.stop().animate({'height':'0','display':'none'},300,'easeInOutCirc',function(){
+			$('.text2').stop().animate({'opacity': '1','display':'block'},300);
 		});
 	});
 
 	$(".fa ,.en ,.about").mouseover(function(){
-		console.log($(this).attr("class"));
 		top.find('p#'+$(this).attr("class")).css({'display':'inline-block'});
 		top.find('p#'+$(this).attr("class")).stop().animate({'opacity': '1'},800);
 	});
@@ -30,15 +26,14 @@ $(function(){
 		top.find('p#'+$(this).attr("class")).stop().animate({'opacity': '0'},50,null,function(){
 			top.find('p#'+$(this).attr("class")).css({'display':'none'});
 		});
-	});
-
-	
+	});	
 /* /////////////////////////////////// Start video jq /////////////////////////////////// */
-	var video = $('.video');
+	
 	var start_intro = $('.start-intro');
-	var close_video = $('.close-video')
-	var overlay = $('.overlay');
-	var myVideo=document.getElementById("myvideo");
+	var close_video = $('.close-video');
+	var myvideo 	= document.getElementById("myvideo");//why?->because myvideo.pause() is in javaScript :-?
+	var overlay 	= $('.overlay');
+	var video 		= $('.video');
 	start_intro.click(function(){
 		overlay.css({'display' : 'block'});
 		overlay.stop().animate({'opacity' : '1'},300,null,function(){
@@ -54,8 +49,8 @@ $(function(){
 			});
 		});
 	});
-	function act(){
-		myVideo.pause();
+	function hideWin(){
+		myvideo.pause();
 		$('.animate').stop().animate({'opacity' : '0'},500,null,function(){
 			overlay.stop().animate({'opacity' : '0'},300,null,function(){
 				close_video.stop().animate({'height' : '22px'},1);
@@ -68,12 +63,10 @@ $(function(){
 		});
 	}
 	close_video.find('.close').click(function(){
-		act();
+		hideWin();
 	});
 	$(document).keydown(function(e){
-		if ((e.keyCode ? e.keyCode : e.which)=="27"){
-			act();
-		}
+		((e.keyCode ? e.keyCode : e.which)=="27")&&hideWin();
 	});
 
 /* /////////////////////////////////// Start page fa /////////////////////////////////// */
